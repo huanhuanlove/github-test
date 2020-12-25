36b87661059c93564b180f6777f8ccdef9ef3fdf
kanghuaian <1468140258@qq.com>
2020-12-25 16:55:17 +0800

    新增role控制类

---
+++ src/main/java/com/kang/github/controller/RoleController.java
@@ -0,0 +1,19 @@
+package com.kang.github.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: kang
+ * @Company: 康康小课堂
+ * @DateTime: 2020-12-25-16:54
+ * @Description: 描述
+ */
+@RestController
+@RequestMapping("role")
+public class RoleController {
+
+    public void fun1() {
+        System.out.println("fun1");
+    }
+}
