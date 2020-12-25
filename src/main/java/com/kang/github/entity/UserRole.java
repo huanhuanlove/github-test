022a587a94bc1db4cc75527d8acee93736ac926f
kanghuaian <1468140258@qq.com>
2020-12-25 16:41:15 +0800

    新增userRole实体类

---
+++ src/main/java/com/kang/github/entity/UserRole.java
@@ -0,0 +1,17 @@
+package com.kang.github.entity;
+
+import lombok.Data;
+
+/**
+ * @author: kang
+ * @Company: 康康小课堂
+ * @DateTime: 2020-12-25-16:40
+ * @Description: 描述
+ */
+@Data
+public class UserRole {
+
+    private Long id;
+
+    private String name;
+}
