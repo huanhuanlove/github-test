327aefe4b4247a804104ed256fd5d2044d2cabf3
kanghuaian <1468140258@qq.com>
2020-12-25 16:34:59 +0800

    新增dev分支 新增role实体类

---
+++ src/main/java/com/kang/github/entity/Role.java
@@ -0,0 +1,17 @@
+package com.kang.github.entity;
+
+import lombok.Data;
+
+/**
+ * @author: kang
+ * @Company: 康康小课堂
+ * @DateTime: 2020-12-25-16:33
+ * @Description: 描述
+ */
+@Data
+public class Role {
+
+    private Long id;
+
+    private String name;
+}
